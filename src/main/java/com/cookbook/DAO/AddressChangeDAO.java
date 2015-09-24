51641744bd63a56b5f3aecbf1a787655589bfced
FranklinMaQ <marcelinaplaneta@interia.pl>
2015-09-24 10:16:55 +0200

    Poprawiono z PersistenceContext na PersistenceUnit

--- src/main/java/com/cookbook/DAO/AddressChangeDAO.java
+++ src/main/java/com/cookbook/DAO/AddressChangeDAO.java
@@ -4,23 +4,29 @@ import com.cookbook.ENGINE.EngineAddress;
 import com.cookbook.ENGINE.EnginePersons;
 import java.util.ArrayList;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceException;
+import javax.persistence.PersistenceUnit;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AddressChangeDAO {
 
-    @PersistenceContext
-    private EntityManager em;
+    @PersistenceUnit
+    private EntityManagerFactory emf;
 
     public AddressChangeDAO() {
     }
 
     public boolean AddAddressToPerson(EnginePersons person, ArrayList<EngineAddress> address) {
+        EntityManager em=emf.createEntityManager();
         try {
             person.setAddressList(address);
+            em.getTransaction().begin();
             em.persist(person);
+            em.flush();
+            em.getTransaction().commit();
         } catch (PersistenceException p) {
             //log4j.error(blablabla+p);
             return false;
--- src/main/java/com/cookbook/DAO/PersonChangeDAO.java
+++ src/main/java/com/cookbook/DAO/PersonChangeDAO.java
@@ -4,19 +4,25 @@ import org.springframework.stereotype.Component;
 import com.cookbook.ENGINE.EnginePersons;
 import com.cookbook.Other.Loger;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceException;
+import javax.persistence.PersistenceUnit;
 import javax.persistence.Query;
 
 @Component
 public class PersonChangeDAO {
 
-    @PersistenceContext
-    private EntityManager em;
+   @PersistenceUnit
+    private EntityManagerFactory emf;
 
     public boolean AddPerson(EnginePersons person) {
+              EntityManager em=emf.createEntityManager();
         try {
+            em.getTransaction().begin();
             em.persist(person);
+            em.flush();
+            em.getTransaction().commit();
         } catch (PersistenceException p) {
             Loger.logger.error("Błąd dodawania osoby");
             return false;
@@ -25,6 +31,7 @@ public class PersonChangeDAO {
     }
 
     public boolean updatePersonFirstname(EnginePersons p, String newName) {
+        EntityManager em=emf.createEntityManager();
         String hqlUpdate = "update EnginePersons c set c.firstname = :newName where c.personId = :id";
         Query x = em.createQuery(hqlUpdate)
                 .setParameter("newName", newName)
@@ -34,6 +41,7 @@ public class PersonChangeDAO {
     }
 
     public boolean deletePerson(EnginePersons p) {
+       EntityManager em=emf.createEntityManager();
         String hqlDelete = "delete EnginePersons where id = :parId";
         int deletedEntities;
         deletedEntities = em.createQuery(hqlDelete)
